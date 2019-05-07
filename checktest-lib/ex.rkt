2ea5efe232fd4b65887f79b53699be94e31243ba
Ryan Culpepper <ryanc@racket-lang.org>
2019-05-07 15:33:28 +0200

    ... ex

--- checktest-lib/ex.rkt
+++ checktest-lib/ex.rkt
@@ -41,3 +41,22 @@
         (test #:name "too"
               (test #:name "nested"
                     (check-equal 'a 'a)))))
+
+(test #:name "printing"
+  (check-equal 1
+               '(test x #:name "suite"
+                  (tests
+                   (check-equal 1 1)
+                   (check-equal 2 2)
+                   (check-equal 3 3)
+                   (test #:pre (skip-test) (check-equal 4 5)))
+                  (test #:name "way"
+                    (test #:name "too"
+                      (test #:name "nested"
+                        (check-equal 'a 'a)))))))
+
+
+(require racket/list)
+(test #:name "printer2"
+  (define xs (range 1000))
+  (check-equal xs (cdr xs)))
